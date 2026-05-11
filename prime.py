75cc68ea88356cf768470f1c11ed7cefa2cf0183
kruyb <keyurbhandare04@gmail.com>
2026-05-11 16:20:00 +0530

    Refactor prime number check for clarity

--- prime.py
+++ prime.py
@@ -1,18 +1,12 @@
 n = int(input("What is n? "))
-
 if n < 2:
     print(f"{n} is not prime")
-elif n == 2:
-    print(f"{n} is prime")
-elif n % 2 == 0:
-    print(f"{n} is not prime")
+elif n == 2 or n % 2 == 0:
+    print(f"{n} is not prime" if n != 2 else f"{n} is prime")
 else:
     verifier = False
     for i in range(3, int(pow(n, 0.5)) + 1, 2):
         if n % i == 0:
             verifier = True
             break
-    if verifier:
-        print(f"{n} is not prime")
-    else:
-        print(f"{n} is prime")
+    print(f"{n} is not prime" if verifier else f"{n} is prime")
